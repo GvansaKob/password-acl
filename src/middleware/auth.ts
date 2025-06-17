a8d7a75776b223038c1d0ab426651c46917e61cd
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-17 16:25:16 +0200

    auth des tokens

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,5 @@
 import { verifyAccessToken } from '../services/JWT.js';
 import { createMiddleware } from 'hono/factory';
-
 import { users } from '../fake/data.js';
 
 export const authMiddleware = createMiddleware(async (c, next) => {
@@ -13,22 +12,15 @@ export const authMiddleware = createMiddleware(async (c, next) => {
 
     const token = authHeader.split(' ')[1];
     const decoded = verifyAccessToken(token);
-
-    // Récupérer l'utilisateur depuis la "base de données"
     const user = users.get(decoded.username);
 
     if (!user) {
       return c.json({ error: 'Utilisateur non trouvé' }, 404);
     }
 
-    // Stocker les informations de l'utilisateur dans le contexte
-    c.set('user', {
-      ...user,
-      id: decoded.userId
-    });
-
+    c.set('user', { ...user, id: decoded.userId });
     await next();
   } catch (error) {
     return c.json({ error: 'Token invalide ou expiré' }, 401);
   }
-});  
+});
