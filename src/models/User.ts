aa98886fba8b293b80c159535185e816767c697f
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-17 16:23:59 +0200

    modèle user - stockage

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,17 @@
-
 import { z } from 'zod';
 import { users } from '../fake/data.js';
 
-export const UserSchema = z.object({
+export const RegisterSchema = z.object({
   username: z.string().min(3).max(20),
   password: z.string().min(8).regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
   role: z.enum(['admin', 'user', 'guest']),
   email: z.string().email()
+});
+
+export const UserSchema = RegisterSchema.extend({
+  id: z.string(),
+  salt: z.string(),
+  password: z.string()
 });
 
 export const LoginSchema = z.object({
@@ -16,14 +21,14 @@ export const LoginSchema = z.object({
 
 export type User = z.infer<typeof UserSchema>;
 
-export function getUser(id: string) {
-  return users.get(id);
+export function getUser(username: string): User | undefined {
+  return users.get(username);
 }
 
 export function setUser(user: User) {
   users.set(user.username, user);
 }
 
-export function userExist(id: string) {
-  return users.has(id);
+export function userExist(username: string): boolean {
+  return users.has(username);
 }
