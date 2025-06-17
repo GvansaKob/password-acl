8d5a8d0060997cc056ec229a4fb3b1ca7a3d714e
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-17 16:26:06 +0200

    initialiser différents rôles

--- src/services/accessControl.ts
+++ src/services/accessControl.ts
@@ -2,6 +2,27 @@ import { AccessControl } from 'accesscontrol';
 
 const ac = new AccessControl();
 
-// Définissez vos permissions ici
+ac.grant('admin')
+  .createAny('profile')
+  .readAny('profile')
+  .updateAny('profile')
+  .deleteAny('profile')
+  .createAny('data')
+  .readAny('data')
+  .updateAny('data')
+  .deleteAny('data');
+
+ac.grant('user')
+  .createOwn('profile')
+  .readOwn('profile')
+  .updateOwn('profile')
+  .deleteOwn('profile')
+  .createOwn('data')
+  .readOwn('data')
+  .updateOwn('data')
+  .deleteOwn('data');
+
+ac.grant('guest')
+  .readAny('profile');
 
 export { ac };
